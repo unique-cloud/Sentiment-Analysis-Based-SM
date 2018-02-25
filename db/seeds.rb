5c0dd0c2c7c051fe52f99e5cde142c32b4cb4eeb
Abhishek Pandey <aapandey@ncsu.edu>
2018-02-25 02:25:04 -0500

    Added edit,delete,update functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
+             email: "exampleasd@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
+  email = "example-#{n+1}@railstutorial#{n+1}.org"
   password = "password"
   User.create!(name:  name,
                email: email,
