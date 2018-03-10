0a89d7a4c0ba21471a6fca051ac332ab90a0fd97
unique_cloud <xtang9@ncsu.edu>
2018-03-10 02:13:50 -0500

    fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-User.create!(name:  "Example User",
-             email: "exampleasd@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             admin: true,
-             activated: true,
-             activated_at: Time.zone.now)
-
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial#{n+1}.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now)
-end
-
+# Users
 User.create!(name: "Example User",
              email: "example@railstutorial.org",
              password: "foobar",
@@ -32,6 +6,7 @@ User.create!(name: "Example User",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
+
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
@@ -44,6 +19,7 @@ User.create!(name: "Example User",
                activated_at: Time.zone.now)
 end
 
+# Microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
