8ac954b01dc445b5bb459a1af86e78fe2bd8e702
pmishra <piyushmishra0894@gmail.com>
2018-03-11 19:46:34 -0400

    algo.py modified

--- LDA-TopicModeling/algo.py
+++ LDA-TopicModeling/algo.py
@@ -8,16 +8,69 @@ import json
 
 app = Flask(__name__)
 
-'''
 # @ signfies a decorator : way to wrap a function and modifying its behaviour
-@app.route('/')
+@app.route('https://ocean-ncsu.herokuapp.com/combinations', methods=['POST'])
 def index():
-    return '<h2>This is a website</h2>'
 
+    #contentJSON = request.JSON
+    data = {}
+    data['micropost_id'] = 1
+    data['content'] = "Lorem ipsum"
+    json_data = json.dumps(data)
+
+    params = {}
+    params['micropost_id'] = 1
+    #{ params: { micropost_id: integer(same), tags: string array (["test1", "test2", "test3"]) }
+
+    #lda
+    tokenizer = RegexpTokenizer(r'\w+')
+
+    # create English stop words list
+    en_stop = get_stop_words('en')
+
+    # Create p_stemmer of class PorterStemmer
+    p_stemmer = PorterStemmer()
+
+    # create sample documents
+    doc_a = data['content']
+    # compile sample documents into a list
+    doc_set = [doc_a]
+
+    # list for tokenized documents in loop
+    texts = []
+
+    # loop through document list
+    for i in doc_set:
+        # clean and tokenize document string
+        raw = i.lower()
+        tokens = tokenizer.tokenize(raw)
+
+        # remove stop words from tokens
+        stopped_tokens = [i for i in tokens if not i in en_stop]
+
+        # stem tokens
+        stemmed_tokens = [p_stemmer.stem(i) for i in stopped_tokens]
+
+        # add tokens to list
+        texts.append(stemmed_tokens)
+
+    # turn our tokenized documents into a id <-> term dictionary
+    dictionary = corpora.Dictionary(texts)
+
+    # convert tokenized documents into a document-term matrix
+    corpus = [dictionary.doc2bow(text) for text in texts]
+
+    # generate LDA model
+    ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=2, id2word=dictionary, passes=1500)
+    params["tags"] = json.dumps(ldamodel.print_topics(num_topics=2, num_words=3))
+
+    json_data = json.dumps(params)
+    return params
+
+'''
 @app.route('/profile<username>')
 def profile(username):
     return '<h2>Welcome %s</h2>' %username
-'''
 tokenizer = RegexpTokenizer(r'\w+')
 
 # create English stop words list
@@ -59,6 +112,6 @@ corpus = [dictionary.doc2bow(text) for text in texts]
 # generate LDA model
 ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=2, id2word=dictionary, passes=1500)
 print json.dumps(ldamodel.print_topics(num_topics=2, num_words=3))
-
-#if __name__ == "__main__":
- #   app.run(debug=True)
+'''
+if __name__ == "__main__":
+   app.run(debug=True)
