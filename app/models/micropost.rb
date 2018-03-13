bf498763c1a8866ae8aebea297ba21d6186d14b9
unique_cloud <xtang9@ncsu.edu>
2018-03-12 23:35:19 -0400

    Change maximum limit for post

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,7 +6,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
-  validates :content, presence: true, length: { maximum: 140 }
+  validates :content, presence: true, length: { maximum: 1000 }
 
   def combine_tag(tag)
     unless tags.include?(tag)
